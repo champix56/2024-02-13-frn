282e9231802db2daf678185264655c52acf80f78
champix56 <desorbaix@free.fr>
2024-02-13 14:18:48 +0100

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './App.styles';
+
+function App() {
+  return (
+    <View>
+      <Text style={styles.demat}>DEMAT Breizh</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ src/App/App.styles.jsx
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  demat: {
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
