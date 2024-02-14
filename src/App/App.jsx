72aa52d6826a97dba2cd947cc6149d4295ec8805
champix56 <desorbaix@free.fr>
2024-02-14 14:46:38 +0100

    mise en oeuvre du layout cmp

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import styles from './App.styles';
 import Button from './components/ui/Button/Button';
+import DocInList from './components/ui/DocInList/DocInList';
+import ListDoc from './components/layout/ListDoc/ListDoc';
 
 function App() {
   const [counter, setCounter] = useState(-1);
@@ -36,6 +38,18 @@ function App() {
           console.log(counter);
         }}
       />
+      <ListDoc>
+        <DocInList type="PDF" date="2024-01-01" titre="Document 1" />
+        <DocInList type="IMG" titre="Document 2" />
+        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
+        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
+        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
+        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
+        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
+        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
+        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
+        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
+      </ListDoc>
     </View>
   );
 }
