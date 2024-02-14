8d2760ca7b7c786d28134936f1e536a3215ca29a
champix56 <desorbaix@free.fr>
2024-02-14 14:46:05 +0100

    docInList -> ts

--- src/App/components/ui/DocInList/DocInList.styles.jsx
+++ src/App/components/ui/DocInList/DocInList.styles.jsx
@@ -1,6 +1,18 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  DocInList: {},
+  DocInList: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 7,
+    marginBottom: 5,
+    backgroundColor: 'white',
+  },
+  container: {flexDirection: 'row'},
+  imageContainer: {padding: 10},
+  icon: {width: 72, height: 72},
+  textContainer: {flexGrow: 1, alignItems: 'center', justifyContent: 'center'},
+  titre: {fontWeight: '900', fontSize: 24},
+  date: {},
 });
 export default styles;
---
+++ src/App/components/ui/DocInList/DocInList.tsx
@@ -0,0 +1,41 @@
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import styles from './DocInList.styles';
+/**
+ * Composant DocInList
+ * @param {*} props props du composant DocInList
+ * @returns {React.ReactNode} structure d'affichage components de DocInList
+ */
+interface IDocInListProps {
+  titre: string;
+  type: 'PDF' | 'IMG' | 'PPT';
+  date?: string;
+}
+
+const DocInList: React.FC<IDocInListProps> = props => {
+  let icon = require('../../../../../assets/img/icon_img.png');
+  switch (props.type) {
+    case 'PDF':
+      icon = require('../../../../../assets/img/icon_pdf.png');
+      break;
+    case 'PPT':
+      icon = require('../../../../../assets/img/icon_ppt.png');
+      break;
+  }
+  return (
+    <TouchableOpacity style={styles.DocInList} testID="DocInList">
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={icon} style={styles.icon} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.titre}>{props.titre}</Text>
+          {undefined !== props.date && (
+            <Text style={styles.date}>en date du :{props.date}</Text>
+          )}
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+export default DocInList;
