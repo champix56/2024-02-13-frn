12286f61801cdbcff867a01fde9e97f2528c569e
champix56 <desorbaix@free.fr>
2024-02-14 15:09:04 +0100

    vidange app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,38 +6,18 @@ import DocInList from './components/ui/DocInList/DocInList';
 import ListDoc from './components/layout/ListDoc/ListDoc';
 
 function App() {
-  const [counter, setCounter] = useState(-1);
+  const [state, setsetState] = useState([]);
   useEffect(() => {
     /*componentDidMount*/
-    setCounter(0);
+
     return () => {
       //componentWillUnmout
       console.log('unmountApp');
     };
   }, []);
-  useEffect(() => {
-    console.log(counter);
-  }, [counter]);
 
   return (
     <View>
-      <Text style={styles.demat}>counter value :{counter}</Text>
-      <Button
-        text="-1"
-        bgColor="red"
-        onButtonPress={evt => {
-          setCounter(counter - 1);
-          console.log(counter, evt);
-        }}
-      />
-
-      <Button
-        text="+1"
-        onButtonPress={e => {
-          setCounter(counter + 1);
-          console.log(counter);
-        }}
-      />
       <ListDoc>
         <DocInList type="PDF" date="2024-01-01" titre="Document 1" />
         <DocInList type="IMG" titre="Document 2" />
