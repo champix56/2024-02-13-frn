e6974b385066d9bd9a6fe98524bc6b6605585a12
champix56 <desorbaix@free.fr>
2024-02-14 17:24:40 +0100

    gestion in cmp

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 import styles from './App.styles';
 import Button from './components/ui/Button/Button';
 import DocInList from './components/ui/DocInList/DocInList';
 import ListDoc from './components/layout/ListDoc/ListDoc';
+import {REST_RESSOURCES, REST_URL} from './config/config';
 
 function App() {
-  const [state, setsetState] = useState([]);
+  const [user, setUser] = useState({});
+  const [documents, setDocuments] = useState([]);
+  const [filterValue, setfFilterValue] = useState('');
   useEffect(() => {
     /*componentDidMount*/
+    const pruser = fetch(`${REST_URL}${REST_RESSOURCES.users}/0`).then(r => {
+      console.log(r);
+      return r.json();
+    });
+    const prdoc = fetch(
+      `${REST_URL}${REST_RESSOURCES.documents}?userId=0`,
+    ).then(r => {
+      console.log(r);
+      return r.json();
+    });
+
+    Promise.all([pruser, prdoc]).then(arrContenus => {
+      setUser(arrContenus[0]);
+      setDocuments(arrContenus[1]);
+    });
 
     return () => {
       //componentWillUnmout
@@ -18,17 +36,28 @@ function App() {
 
   return (
     <View>
+      <Text>
+        {user.name} {user.firstName} / {documents.length} / {filterValue}
+      </Text>
+      <TextInput
+        placeholder="recherche"
+        value={filterValue}
+        onChangeText={newValue => {
+          setfFilterValue(newValue);
+        }}
+      />
       <ListDoc>
-        <DocInList type="PDF" date="2024-01-01" titre="Document 1" />
-        <DocInList type="IMG" titre="Document 2" />
-        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
-        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
-        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
-        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
-        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
-        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
-        <DocInList type="PDF" titre="PDF 24" date="2024-01-04" />
-        <DocInList type="PPT" titre="Powerpoint" date="2024-01-04" />
+        <DocInList type="PDF" titre="Nouveau" />
+        {documents
+          .filter(e => {
+            return (
+              e.type.toLowerCase().includes(filterValue.toLowerCase()) ||
+              e.titre.toLowerCase().includes(filterValue.toLowerCase())
+            );
+          })
+          .map((e, i) => {
+            return <DocInList key={'dil' + e.id} {...e} />;
+          })}
       </ListDoc>
     </View>
   );
