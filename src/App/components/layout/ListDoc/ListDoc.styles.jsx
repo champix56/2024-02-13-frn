4f6a05f8482faa39d6ec2a2bdc4c8f52019d04ad
champix56 <desorbaix@free.fr>
2024-02-15 01:04:50 +0100

    fixHeight to listDoc

--- src/App/components/layout/ListDoc/ListDoc.styles.jsx
+++ src/App/components/layout/ListDoc/ListDoc.styles.jsx
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  ListDoc: {padding: 5, backgroundColor: '#B0B0B0'},
+  ListDoc: {padding: 5, backgroundColor: '#B0B0B0', height: 350},
 });
 export default styles;
